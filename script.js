b580644fc226803bb84a65d92c6e0dadeb8c7ec0
erikdediosd <erikdediosd.cejsm@gmail.com>
2024-01-17 09:24:33 -0600

    Update 2.0 Minor responsive n' effects changes

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ burger.addEventListener('click', function(){
     burger.classList.toggle('active')
     nav.classList.toggle('active')
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navbar");
+    const containersWithLightbg = document.querySelectorAll(".lightbg-container");
+  
+    window.addEventListener("scroll", function () {
+      let shouldAddClass = false;
+  
+      containersWithLightbg.forEach((container) => {
+        const containerTop = container.offsetTop;
+        const containerBottom = containerTop + container.clientHeight;
+  
+        if (window.scrollY >= containerTop && window.scrollY < containerBottom) {
+          shouldAddClass = true;
+        }
+      });
+  
+      if (shouldAddClass) {
+        navbar.classList.add("lightbg");
+      } else {
+        navbar.classList.remove("lightbg");
+      }
+    });
+  });
